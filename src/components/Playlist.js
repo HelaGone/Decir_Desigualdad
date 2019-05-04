0ed702b61188ea757fc447b7137910366230d89b
hela <luah613@gmail.com>
2019-05-04 17:12:42 -0500

    wrapping up

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -3,9 +3,18 @@ import ListItem from './ListItem';
 export default class Playlist extends Component{
 	render(){
 		const {episodios, methods, playerStatus, playThisEpisode, type} = this.props;
+		let section_title = '';
+		if(type.toUpperCase()==='EPISODIO'){
+			section_title = `${type.toUpperCase()}S`;
+		}else{
+			section_title = type.toUpperCase();
+		}
 		return(
 			<Fragment>
 				<ul className="playlist">
+				<li className="audio_item">
+					<h3>{ section_title }</h3>
+				</li>
 					{
 						episodios.map(episodio=>{
 							return (
